6437386a5457b10c89bf8d8d1619d8b3211d2419
Chuck Lantz <clantz@microsoft.com>
2020-10-04 18:03:35 +0000

    Tweaks

--- clone-working-repos.sh
+++ clone-working-repos.sh
@@ -2,6 +2,5 @@
 
 # Go to the workspace folder
 cd $(dirname $0)/..
-git-credential-manager-core configure
 git clone https://github.com/microsoft/vscode-dev-containers
 git clone https://github.com/microsoft/vscode-remote-containers
