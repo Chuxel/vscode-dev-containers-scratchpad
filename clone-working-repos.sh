85662ad69495dd0a72f4064dbcd81db1ed926942
Chuck Lantz <clantz@microsoft.com>
2020-10-04 18:57:57 +0000

    Fixes

--- clone-working-repos.sh
+++ clone-working-repos.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+clone-repo()
+{
+    REPO=$1
+    cd $(dirname $0)/..
+    mkdir -p ${REPO}
+    cd ${REPO}
+    git config credential.helper store
+    git clone https://github.com/microsoft/${REPO} .
+}
 
-# Go to the workspace folder
-cd $(dirname $0)/..
-git clone https://github.com/microsoft/vscode-dev-containers
-git clone https://github.com/microsoft/vscode-remote-containers
+clone-repo vscode-dev-containers
+clone-repo vscode-remote-release
