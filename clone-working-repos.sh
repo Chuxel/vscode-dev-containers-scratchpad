80c26aa852f08ae090f0e1ef093100efaba3a99c
Chuck Lantz <clantz@microsoft.com>
2020-10-04 19:18:59 +0000

    Fix

--- clone-working-repos.sh
+++ clone-working-repos.sh
@@ -2,8 +2,12 @@
 clone-repo()
 {
     cd $(dirname $0)/..
-    git clone -c credential.helper=store https://github.com/microsoft/$1 
+    if [ ! -d "$1" ]; then
+        git clone -c credential.helper=store https://github.com/microsoft/$1 
+    else 
+        echo "Already cloned $1"
+    fi
 }
 
 clone-repo vscode-dev-containers
-clone-repo vscode-remote-release
+clone-repo vscode-remote-containers
