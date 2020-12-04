dbdeeceb89f98a496e65721cade4485904f1b94c
Chuck Lantz <clantz@microsoft.com>
2020-12-04 22:51:59 +0000

    Tweak to clone

--- clone-working-repos.sh
+++ clone-working-repos.sh
@@ -4,13 +4,15 @@ clone-repo()
 {
     cd "${WORKSPACES}"
     if [ ! -d "$1" ]; then
-        git clone -c credential.helper=store https://github.com/microsoft/$1 
+        #git clone -c credential.helper=store https://github.com/microsoft/$1 
+        git clone https://github.com/microsoft/$1 
     else 
         echo "Already cloned $1"
     fi
 }
 
 sudo sed -i -E 's/helper =.*//' /etc/gitconfig
+git config --global credential.helper '!f() { sleep 1; echo "username=${GH_USER}"; echo "password=${GH_TOKEN}"; }; f'
 
 clone-repo vscode-dev-containers
 clone-repo vscode-remote-containers
