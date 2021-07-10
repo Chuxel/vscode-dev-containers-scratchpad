7447261d058bafb0708b6d78d903cd31ac28e11b
Chuck Lantz <chuck_lantz@hotmail.com>
2021-07-10 23:46:10 +0000

    Handle symlinks existing

--- clone-working-repos.sh
+++ clone-working-repos.sh
@@ -16,20 +16,20 @@ git config --global credential.helper '!f() { sleep 1; echo "username=${GH_USER}
 clone-repo vscode-remote-containers
 clone-repo vscode-dev-containers
 mkdir -p "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos"
-ln -s "${WORKSPACES}/vscode-dev-containers" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-dev-containers" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-node
-ln -s "${WORKSPACES}/vscode-remote-try-node" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-node" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-python
-ln -s "${WORKSPACES}/vscode-remote-try-python" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-python" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-go
-ln -s "${WORKSPACES}/vscode-remote-try-go" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-go" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-java
-ln -s "${WORKSPACES}/vscode-remote-try-java" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-java" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-dotnetcore
-ln -s "${WORKSPACES}/vscode-remote-try-dotnetcore" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-dotnetcore" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-rust
-ln -s "${WORKSPACES}/vscode-remote-try-rust" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-rust" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-cpp
-ln -s "${WORKSPACES}/vscode-remote-try-cpp" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-cpp" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
 clone-repo vscode-remote-try-php
-ln -s "${WORKSPACES}/vscode-remote-try-php" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
+ln -sf "${WORKSPACES}/vscode-remote-try-php" "${WORKSPACES}/vscode-remote-containers/test/dev-containers/repos/"
